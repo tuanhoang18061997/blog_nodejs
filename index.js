367d546bc3337c71b7c93e447daf11e6fc2b9d32
TuanTran <tuanhoang18061997>
2022-01-09 00:38:54 +0700

    test 2

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
-app.get('/test', (req,res) => {
-    res.send('Hello world')
+app.get('/', (req,res) => {
+    res.send('Hello world');
+    console.log("Test git")
 })
 
 app.listen(PORT,() => console.log(`Example app listening at http://localhost:${PORT}`))
